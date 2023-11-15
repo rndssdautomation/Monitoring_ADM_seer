ba89d6612c53c9538d508226ca6c4f24d2adee80
R&D <R&D@DESKTOP-3JSHF7K>
2023-11-15 08:49:56 +0700

    Base

---
+++ app/Controllers/Monitor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Monitor extends BaseController
+{
+    public function index()
+    {
+        return view('Basecore/index');
+    }
+}
---
+++ app/Views/Basecore/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<heade>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Monitoring ADM</title>
+    <link rel="stylesheet" href="<?= base_url();?>/asset/css/bootstrap.min.css">
+</head>
+<body>
+    
+
+    <script src="<?= base_url();?>/asset/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
