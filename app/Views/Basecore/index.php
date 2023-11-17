a13cb047d3127ace96a346e63ce9d58b89284be4
fazanazhar <fazhar40@gmail.com>
2023-11-17 10:17:11 +0700

    ui

--- app/Views/Basecore/index.php
+++ app/Views/Basecore/index.php
@@ -1,13 +1,298 @@
 <!DOCTYPE html>
-<heade>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Monitoring ADM</title>
-    <link rel="stylesheet" href="<?= base_url();?>/asset/css/bootstrap.min.css">
-</head>
-<body>
-    
+<html lang="en">
 
-    <script src="<?= base_url();?>/asset/js/bootstrap.bundle.min.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>ADM SEER</title>
+    <meta property="og:title" content="Superficial Burly Dog" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" />
+    <meta property="twitter:card" content="summary_large_image" />
+
+    <style data-tag="reset-style-sheet">
+    html {
+        line-height: 1.15;
+    }
+
+    body {
+        margin: 0;
+    }
+
+    * {
+        box-sizing: border-box;
+        border-width: 0;
+        border-style: solid;
+    }
+
+    p,
+    li,
+    ul,
+    pre,
+    div,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    figure,
+    blockquote,
+    figcaption {
+        margin: 0;
+        padding: 0;
+    }
+
+    button {
+        background-color: transparent;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+        font-family: inherit;
+        font-size: 100%;
+        line-height: 1.15;
+        margin: 0;
+    }
+
+    button,
+    select {
+        text-transform: none;
+    }
+
+    button,
+    [type="button"],
+    [type="reset"],
+    [type="submit"] {
+        -webkit-appearance: button;
+    }
+
+    button::-moz-focus-inner,
+    [type="button"]::-moz-focus-inner,
+    [type="reset"]::-moz-focus-inner,
+    [type="submit"]::-moz-focus-inner {
+        border-style: none;
+        padding: 0;
+    }
+
+    button:-moz-focus,
+    [type="button"]:-moz-focus,
+    [type="reset"]:-moz-focus,
+    [type="submit"]:-moz-focus {
+        outline: 1px dotted ButtonText;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: inherit;
+    }
+
+    input {
+        padding: 2px 4px;
+    }
+
+    img {
+        display: block;
+    }
+
+    html {
+        scroll-behavior: smooth
+    }
+    </style>
+    <style data-tag="default-style-sheet">
+    html {
+        font-family: Inter;
+        font-size: 16px;
+    }
+
+    body {
+        font-weight: 400;
+        font-style: normal;
+        text-decoration: none;
+        text-transform: none;
+        letter-spacing: normal;
+        line-height: 1.15;
+        color: var(--dl-color-gray-black);
+        background-color: var(--dl-color-gray-white);
+
+    }
+    </style>
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff1.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff2.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff3.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff4.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff5.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff6.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff7.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff8.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff9.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff10.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff11.css" data-tag="font" />
+    <link rel="stylesheet" href="<?= base_url();?>asset/font/ff12.css" data-tag="font" />
+</head>
+
+<body>
+    <link rel="stylesheet" href="<?= base_url();?>asset/css/style.css" />
+    <div>
+        <link href="<?= base_url();?>asset/css/index.css" rel="stylesheet" />
+
+        <div class="home-container">
+            <div class="home-container01">
+                <header data-role="Accordion" class="home-header">
+                    <img alt="logo" src="<?= base_url();?>asset/icon/ssd.png" class="home-image" />
+                    <div class="home-separator"></div>
+                </header>
+            </div>
+            <div class="home-container02">
+                <div class="home-container03">
+                    <div class="home-container04">
+                        <div class="home-container05">
+                            <div class="home-feature-card">
+                                <div class="home-container06">
+                                    <div class="home-container07">
+                                        <img alt="image" src="<?= base_url();?>asset/icon/amr.png" loading="eager"
+                                            class="home-image1" />
+                                    </div>
+                                </div>
+                                <div class="home-container08">
+                                    <div class="home-container09">
+                                        <h2 class="home-text">Robot 1</h2>
+                                        <div class="home-separator1"></div>
+                                        <div class="home-container10">
+                                            <div class="home-feature-card01">
+                                                <h3 class="home-battrey">
+                                                    <span>Battrey</span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text03">80%</span>
+                                            </div>
+                                            <div class="home-feature-card02">
+                                                <h3 class="home-battrey1">
+                                                    <span>Status</span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text06">Runing</span>
+                                            </div>
+                                            <div class="home-feature-card03">
+                                                <h3 class="home-battrey2">
+                                                    <span>Task </span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text09">PruductBV1</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="home-container11">
+                            <div class="home-feature-card04">
+                                <div class="home-container12">
+                                    <div class="home-container13">
+                                        <img alt="image" src="<?= base_url();?>asset/icon/amr.png" loading="eager"
+                                            class="home-image2" />
+                                    </div>
+                                </div>
+                                <div class="home-container14">
+                                    <div class="home-container15">
+                                        <h2 class="home-text10">Robot 1</h2>
+                                        <div class="home-separator2"></div>
+                                        <div class="home-container16">
+                                            <div class="home-feature-card05">
+                                                <h3 class="home-battrey3">
+                                                    <span>Battrey</span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text13">80%</span>
+                                            </div>
+                                            <div class="home-feature-card06">
+                                                <h3 class="home-battrey4">
+                                                    <span>Status</span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text16">Runing</span>
+                                            </div>
+                                            <div class="home-feature-card07">
+                                                <h3 class="home-battrey5">
+                                                    <span>Task </span>
+                                                    <br />
+                                                </h3>
+                                                <span class="home-text19">PruductBV1</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="home-container17">
+                    <div class="home-container18">
+                        <div class="home-container19">
+                            <div class="home-container20">
+                                <div class="home-container21">
+                                    <h3 class="home-text20">Antrian Tugas</h3>
+                                    <div class="home-separator3"></div>
+                                    <div class="home-container22">
+                                        <div class="home-feature-card08">
+                                            <div class="home-container23">
+                                                <h2 class="home-text21">Lorem ipsum</h2>
+                                                <span>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    In lorem lorem, malesuada in metus vitae, scelerisque
+                                                    accumsan ipsum.
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="home-container24">
+                                    <h3 class="home-text23">Antrian Tugas</h3>
+                                    <div class="home-separator4"></div>
+                                    <div class="home-container25">
+                                        <div class="home-feature-card09">
+                                            <div class="home-container26">
+                                                <h2 class="home-text24">Lorem ipsum</h2>
+                                                <span>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    In lorem lorem, malesuada in metus vitae, scelerisque
+                                                    accumsan ipsum.
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="home-container27">
+                            <div class="home-container28">
+                                <h3 class="home-text26">History Tugas</h3>
+                                <div class="home-separator5"></div>
+                                <div class="home-container29">
+                                    <div class="home-container30">
+                                        <div class="home-feature-card10">
+                                            <div class="home-container31">
+                                                <h2 class="home-text27">Lorem ipsum</h2>
+                                                <span>
+                                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                                    In lorem lorem, malesuada in metus vitae, scelerisque
+                                                    accumsan ipsum.
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
