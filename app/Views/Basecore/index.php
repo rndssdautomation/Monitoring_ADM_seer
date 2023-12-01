9498e0e697e3860fb7b220ce8141b445c9212a79
fazanazhar <fazhar40@gmail.com>
2023-12-01 10:31:03 +0700

    navbar

--- app/Views/Basecore/index.php
+++ app/Views/Basecore/index.php
@@ -45,21 +45,7 @@
                     </div>
                     <div>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav ms-auto   mb-lg-0">
-                                <li class="nav-item dropdown me-3">
-                                    <a class="nav-link active dropdown-toggle" href="#" data-bs-toggle="dropdown"
-                                        aria-expanded="false">
 
-                                        <i class='iconly-boldNotification fs-4 text-gray-600'></i>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                                        <li>
-                                            <h6 class="dropdown-header">Notifications</h6>
-                                        </li>
-                                        <li><a class="dropdown-item">No notification available</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
                             <div class="dropdown">
                                 <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                                     <div class="user-menu d-flex">
@@ -80,16 +66,6 @@
                                     <li>
                                         <h6 class="dropdown-header">Hello, User!</h6>
                                         <hr>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="<?= base_url();?>all">
-                                            <i class="icon-mid bi bi-gear me-2"></i>
-                                            Merged robot data</a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="<?= base_url();?>">
-                                            <i class="icon-mid bi bi-gear me-2"></i>
-                                            Separate robot data</a>
                                     </li>
                                 </ul>
                             </div>
