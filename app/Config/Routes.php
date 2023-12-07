49d97a23d9b94374205d47381372ac8d48b752fc
zulfanurfajris <zulfanurfajris@gmail.com>
2023-12-07 13:13:53 +0700

    SSE FIX ZULFA2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,15 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::index');
 $routes->get('/', 'Monitor::index');
 $routes->get('/all', 'Alldata::index');
 $routes->get('/task', 'Task::task');
-// $routes->match(['get','post'],'/get_api_list_task',   'Monitor::get_api_list_task');
-// $routes->match(['get','post'],'/get_api_data_delivery_success',   'Monitor::get_api_data_delivery_success');
 $routes->match(['get','post'],'/get_api_data_delivery',   'Monitor::get_api_data_delivery');
-// $routes->match(['get','post'],'/get_api_data_history_task',   'Monitor::get_api_data_history_task');
 $routes->match(['get','post'],'/get_api_data_status_robot',   'Monitor::get_api_data_status_robot');
-// $routes->match(['get','post'],'/get_api_data_queue_robot1',   'Monitor::get_api_data_queue_robot1');
-// $routes->match(['get','post'],'/get_api_data_queue_robot2',   'Monitor::get_api_data_queue_robot2');
-// $routes->match(['get','post'],'/get_api_data_queue_robot_all',   'Monitor::get_api_data_queue_robot_all');
